34dd56a30ee5861f3cdca921abcaabdfd66dfbbb
Anish Agnihotri <contact@anishagnihotri.com>
2019-07-28 22:01:02 -0400

    WIP: Page headers

--- src/components/pageHeader/index.js
+++ src/components/pageHeader/index.js
@@ -1,22 +1,14 @@
-import { h } from 'preact';
-import { Link } from 'preact-router/match';
+/* eslint-disable react/prefer-stateless-function */
+import { h, Component } from 'preact';
 import style from './style';
 
-const pageHeader = () => (
-	<header class={style.header}>
-		<div>
-			<p>Something</p>
-		</div>
-		<div>
-			<nav>
-				<Link activeClassName={style.active} href="/">Home</Link>
-				<Link activeClassName={style.active} href="/transactions">Transactions</Link>
-				<Link activeClassName={style.active} href="/statistics">Statistics</Link>
-				<Link activeClassName={style.active} href="/faucet">Faucet</Link>
-				<Link activeClassName={style.active} href="/developer">Developer</Link>
-			</nav>
-		</div>
-	</header>
-);
-
-export default pageHeader;
+export class pageHeader extends Component {
+	render(props) {
+		return (
+			<div class={style.pageHeader}>
+				<h1>{ props.title }</h1>
+				<hr />
+			</div>
+		);
+	}
+}
--- src/routes/transactions/index.js
+++ src/routes/transactions/index.js
@@ -1,10 +1,10 @@
 import { h } from 'preact';
+import pageHeader from '../../components/pageHeader';
 import style from './style';
 
 const Transactions = () => (
 	<div class={style.transactions}>
-		<h1>Transactions</h1>
-		<p>This is the transactions component.</p>
+		<pageHeader title="Transactions" />
 	</div>
 );
 
