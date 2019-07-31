93f14163c040ca83e1f0fbd0bf55fc4be0dd482c
Anish Agnihotri <contact@anishagnihotri.com>
2019-07-30 21:34:31 -0400

    Completed header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,33 +1,113 @@
-import { h } from 'preact';
+import {  h , Component } from 'preact';
 import { Link } from 'preact-router/match';
-// import { Visible } from 'preact-visible';
 import style from './style';
+import { HamburgerButton } from 'react-hamburger-button';
 
-const Header = () => (
-	<header class={style.header}>
-		<div>
-			<Link href="/"><img src="../../assets/librasearch.png" alt="Librasearch logo" /></Link>
-		</div>
-		<div>
-			<nav>
-				<Link activeClassName={style.active} href="/">Home</Link>
-				<Link activeClassName={style.active} href="/transactions">Transactions</Link>
-				<Link activeClassName={style.active} href="/statistics">Statistics</Link>
-				<Link activeClassName={style.active} href="/faucet">Faucet</Link>
-				<Link activeClassName={style.active} href="/developer">Developer</Link>
-			</nav>
-		</div>
-		<div>
-			<nav>
-				<Link activeClassName={style.active} href="/">Home</Link>
-				<Link activeClassName={style.active} href="/transactions">Transactions</Link>
-				<Link activeClassName={style.active} href="/statistics">Statistics</Link>
-				<Link activeClassName={style.active} href="/faucet">Faucet</Link>
-				<Link activeClassName={style.active} href="/developer">Developer</Link>
-			</nav>
-		</div>
-		<button>Press me</button>
-	</header>
-);
+export default class Header extends Component {
 
-export default Header;
+	showMenu(event) {
+		event.preventDefault();
+		
+		this.setState({
+			showMenu: !this.state.showMenu,
+			open: !this.state.open
+		});
+	}
+
+	linkCloseMenu(event) {
+		event.preventDefault();
+
+		this.setState({
+			showMenu: false,
+			open: false
+		});
+	}
+
+	updateDimensions() {
+		if (window.innerWidth > 780) {
+			this.setState({
+				showMenu: false,
+				open: false,
+				showMenuIcon: false
+			});
+		}
+		else {
+			this.setState({
+				showMenuIcon: true
+			});
+		}
+	}
+	
+	constructor() {
+		super();
+		
+		this.state = {
+			showMenu: false,
+			showMenuIcon: true
+		};
+		
+		this.showMenu = this.showMenu.bind(this);
+		this.linkCloseMenu = this.linkCloseMenu.bind(this);
+	}
+
+	componentDidMount() {
+		this.updateDimensions();
+		window.addEventListener('resize', this.updateDimensions.bind(this));
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.updateDimensions.bind(this));
+	}
+
+	render() {
+		return (
+			<header class={style.header}>
+				<div>
+					<Link href="/"><img src="../../assets/librasearch.png" alt="Librasearch logo"/></Link>
+				</div>
+				<div>
+					<nav>
+						<Link activeClassName={style.active} href="/">Home</Link>
+						<Link activeClassName={style.active} href="/transactions">Transactions</Link>
+						<Link activeClassName={style.active} href="/statistics">Statistics</Link>
+						<Link activeClassName={style.active} href="/faucet">Faucet</Link>
+						<Link activeClassName={style.active} href="/developer">Developer</Link>
+					</nav>
+					{
+						this.state.showMenuIcon
+							? (
+								<HamburgerButton
+									open={this.state.open}
+									onClick={this.showMenu}
+									strokeWidth={3}
+									color="#565761"
+									height={17}
+									width={25}
+								/>
+							)
+							: (
+								null
+							)
+					}
+				</div>
+				{
+					this.state.showMenu
+						? (
+							<div>
+								<nav>
+									<Link onClick={this.linkCloseMenu} activeClassName={style.active} href="/">Home</Link>
+									<Link onClick={this.linkCloseMenu} activeClassName={style.active} href="/transactions">Transactions</Link>
+									<Link onClick={this.linkCloseMenu} activeClassName={style.active} href="/statistics">Statistics</Link>
+									<Link onClick={this.linkCloseMenu} activeClassName={style.active} href="/faucet">Faucet</Link>
+									<Link onClick={this.linkCloseMenu} activeClassName={style.active} href="/developer">Developer</Link>
+								</nav>
+							</div>
+						)
+						: (
+							null
+						)
+				}
+			</header>
+		);
+	}
+}
