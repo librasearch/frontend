cf111926e54f60b279db2fa05068aea05f8cea35
Anish Agnihotri <contact@anishagnihotri.com>
2019-08-04 14:44:45 -0400

    WIP table

--- src/components/transactiontable/latest/index.js
+++ src/components/transactiontable/latest/index.js
@@ -2,6 +2,7 @@
 /* eslint-disable react/prefer-stateless-function */
 import {  h , Component } from 'preact';
 import ReactTable from 'react-table';
+import { Link } from 'preact-router/match';
 import moment from 'moment';
 import axios from 'axios';
 import style from './style';
@@ -13,9 +14,6 @@ export default class LatestTransactionTable extends Component {
 			.then(response => {
 				const result = response.data; // Setup response.data as result variable
 				for (let i = 0; i < result.length; i++) {
-
-					// Setup additional property for link
-					result[i].link = result[i].from;
 
 					// Add mint / p2p option.
 					if (result[i].from === '0000000000000000000000000000000000000000000000000000000000000000') {
@@ -47,7 +45,7 @@ export default class LatestTransactionTable extends Component {
 
 		this.state = {
 			latestTransactions: [], // Initializing empty latestTransactions array
-			tableColumns: [{ Header: 'TX ID',accessor: '_id' },{ Header: 'EXPIRATION TIME',accessor: 'time' },{ Header: 'FROM',accessor: 'from' },{ Header: 'TO',accessor: 'to' },{ Header: 'AMOUNT',accessor: 'value' },{ Header: 'TX FEE',accessor: 'gas_price' }] // Setting up dynamic table columns
+			tableColumns: [{ Header: 'TX ID',accessor: '_id', Cell: props => <Link href={`/version/${props.value}`}>{props.value}</Link>},{ Header: 'EXPIRATION TIME',accessor: 'time'},{ Header: 'FROM',accessor: 'from', Cell: props => <Link href={`/address/${props.value}`}>{props.value}</Link>},{ Header: 'TO',accessor: 'to', Cell: props => <Link href={`/address/${props.value}`}>{props.value}</Link>},{ Header: 'AMOUNT',accessor: 'value' },{ Header: 'TX FEE',accessor: 'gas_price' }] // Setting up dynamic table columns
 		};
 
 		this.pullLatest = this.pullLatest.bind(this); // Binding this to pullLatest()
@@ -58,17 +56,20 @@ export default class LatestTransactionTable extends Component {
 		this.pullLatest; // First pull from '/latest'
 		setInterval(this.pullLatest, 500);
 	}
-	
+
 	render() {
 		return (
-			<footer class={style.latesttable}>
+			<div>
 				<div className="pageItem">
 					<ReactTable
 						data={this.state.latestTransactions}
 						columns={this.state.tableColumns}
+						defaultPageSize={15}
+						showPagination={false}
+						className={'latesttable'}
 					/>
 				</div>
-			</footer>
+			</div>
 		);
 	}
 }
