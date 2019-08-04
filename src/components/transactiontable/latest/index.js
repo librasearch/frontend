dd39ec76ef2863211f2527f427f8609632e5953d
Anish Agnihotri <contact@anishagnihotri.com>
2019-08-04 13:06:03 -0400

    Working in progress: rendering data

--- src/components/transactiontable/latest/index.js
+++ src/components/transactiontable/latest/index.js
@@ -9,18 +9,19 @@ export default class LatestTransactionTable extends Component {
 	pullLatest() {
 		axios.get('http://localhost:3000/latest')
 			.then(response => {
-				// Stringify response data
-				const result = JSON.stringify(response.data);
+				const result = JSON.stringify(response.data); // Stringify response data
+
 				this.setState({
 					latestTransactions: result // Set latestTransactions to equal resulting data
 				});
+
 				console.log(this.state.latestTransactions);
 			}).catch(err => {
 				// Catch errors if any
 				console.log('Please contact support. There seems to be an error. Error code:' + err);
 			});
 	}
-	
+
 	// Constructor setup
 	constructor() {
 		super();
@@ -35,15 +36,13 @@ export default class LatestTransactionTable extends Component {
 	// On mount component update.
 	componentDidMount() {
 		this.pullLatest; // First pull from '/latest'
-		setInterval(this.pullLatest, 1000); // Set interval to update every second.
+		setInterval(this.pullLatest, 500);
 	}
 	
 	render() {
 		return (
 			<footer class={style.latesttable}>
 				<div className="pageItem">
-					<p>Table here</p>
-					<button onClick={this.check}>Check</button>
 				</div>
 			</footer>
 		);
